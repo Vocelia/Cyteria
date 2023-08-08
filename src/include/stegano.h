671f3a53477fde10d5251efdd5b313891f7bf95e
Vocelia <vocelia@outlook.jp>
2023-08-08 22:01:26 +0300

    Adding a new type

--- src/include/stegano.h
+++ src/include/stegano.h
@@ -3,4 +3,14 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+typedef struct {
+    bool alpha;
+    uint32_t offset;
+    uint32_t* cursor;
+    uint32_t msg_len;
+    uint32_t data_len;
+    const char* msg;
+    unsigned char* data;
+} stegano_t;
+
 bool hide(uint8_t system, uint8_t spacing, unsigned char* data, const char* msg, bool alpha);

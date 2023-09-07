17acc71e7ea331b5d5da37a9bbe21b2985468ed0
Vocelia <vocelia@outlook.jp>
2023-09-07 12:06:46 +0300

    Improving code readability and adding some commentrs

--- src/cipher.c
+++ src/cipher.c
@@ -3,123 +3,124 @@
 #include <stdint.h>
 #include <string.h>
 
-//All system variables the cipher depends on
+/*All system variables the cipher depends on*/
 static const char* SYS_0 = "abcdefghijklmnopqrstuvwxyz";
 static const char* SYS_1 = "abcd01efgh45ijkl23mnopqr89stuv6wxyz7";
 static const char* SYS_2 = "ab cd01ef?,gh45ij@#$kl23mn.%&opqr89stuv6wx!yz7*";
 
-//Spacing range:
+//spacing range:
 //SYS_0 (0~25) | SYS_1 (0~35) | SYS_2 (0~46)
 //Any spacing out of boundary may lead to issues
 //The buffer allocation space should match text or greater
 //The memory block of buffer will be reset in its initial execution
 void encrypt(char* text, char* buffer, uint8_t system, uint8_t spacing) {
-	const char* ptr;
-	unsigned int i, loop;
-	memset(buffer, 0, sizeof(buffer));
-	switch (system) {
-	case 0:
-    for (loop=0; loop<strlen(text); loop++) {
-      if (0x61<=text[loop] && text[loop]<=0x7a) ptr = strchr(SYS_0, text[loop]);
-      else ptr = strchr(SYS_0, text[loop]+0x20);
+  const char* ptr;
+  uint32_t sysi, txti; /*sysi - SYS index, txti - text index*/
+  memset(buffer, 0, sizeof(buffer));
+  switch (system) {
+  case 0:
+    for (txti=0; txti<strlen(text); txti++) {
+      if (islower(text[txti])) ptr = strchr(SYS_0, text[txti]);
+      else ptr = strchr(SYS_0, text[txti]+0x20);
       if (ptr!=NULL) {
-        i = ptr - SYS_0;
-        i += (i+spacing<=26) ? spacing : spacing-27;
-        sprintf(buffer+strlen(buffer), "%c", (0x61<=text[loop] && text[loop]<=0x7a) ? SYS_0[i] : SYS_0[i]-0x20);
-      } else sprintf(buffer+strlen(buffer), "%c", text[loop]);
+        sysi = ptr - SYS_0; /*pointer arithmetic to get character index in SYS*/
+        sysi += (sysi+spacing<=26) ? spacing : spacing-27; /*character encryption process*/
+        sprintf(buffer+strlen(buffer), "%c", (islower(text[txti])) ? SYS_0[sysi] : SYS_0[sysi]-0x20);
+      } else sprintf(buffer+strlen(buffer), "%c", text[txti]);
     }
-	break;
-	case 1:
-    for (loop=0; loop<strlen(text); loop++) {
-      if (isalpha(text[loop])) {
-        ptr = strchr(SYS_1, (0x61<=text[loop] && text[loop]<=0x7a) ? text[loop] : text[loop]+0x20);
-        i = ptr - SYS_1;
-        i += (i+spacing<=36) ? spacing : spacing-37;
-        if (isalpha(SYS_1[i])) sprintf(buffer+strlen(buffer), "%c", (0x61<=text[loop] && text[loop]<=0x7a) ? SYS_1[i] : SYS_1[i]-0x20);
-        else sprintf(buffer+strlen(buffer), "%c", SYS_1[i]);
+  break;
+  case 1:
+    for (txti=0; txti<strlen(text); txti++) {
+      if (isalpha(text[txti])) {
+        ptr = strchr(SYS_1, (islower(text[txti])) ? text[txti] : text[txti]+0x20);
+        sysi = ptr - SYS_1;
+        sysi += (sysi+spacing<=36) ? spacing : spacing-37;
+        if (isalpha(SYS_1[sysi])) sprintf(buffer+strlen(buffer), "%c", (islower(text[txti])) ? SYS_1[sysi] : SYS_1[sysi]-0x20);
+        else sprintf(buffer+strlen(buffer), "%c", SYS_1[sysi]);
         continue;
-      } else ptr = strchr(SYS_1, text[loop]);
+      } else ptr = strchr(SYS_1, text[txti]);
       if (ptr!=NULL) {
-        i = ptr - SYS_1;
-        i += (i+spacing<=36) ? spacing : spacing-37;
-        sprintf(buffer+strlen(buffer), "%c", SYS_1[i]);
-      } else sprintf(buffer+strlen(buffer), "%c", text[loop]);
+        sysi = ptr - SYS_1;
+        sysi += (sysi+spacing<=36) ? spacing : spacing-37;
+        sprintf(buffer+strlen(buffer), "%c", SYS_1[sysi]);
+      } else sprintf(buffer+strlen(buffer), "%c", text[txti]);
     }
-	break;
-	case 2:
-    for (loop=0; loop<strlen(text); loop++) {
-      if (isalpha(text[loop])) {
-        ptr = strchr(SYS_2, (0x61<=text[loop] && text[loop]<=0x7a) ? text[loop] : text[loop]+0x20);
-        i = ptr - SYS_2;
-        i += (i+spacing<=46) ? spacing : spacing-47;
-        if (isalpha(SYS_2[i])) sprintf(buffer+strlen(buffer), "%c", (0x61<=text[loop] && text[loop]<=0x7a) ? SYS_2[i] : SYS_2[i]-0x20);
-        else sprintf(buffer+strlen(buffer), "%c", SYS_2[i]);
+  break;
+  case 2:
+    for (txti=0; txti<strlen(text); txti++) {
+      if (isalpha(text[txti])) {
+        ptr = strchr(SYS_2, (islower(text[txti])) ? text[txti] : text[txti]+0x20);
+        sysi = ptr - SYS_2;
+        sysi += (sysi+spacing<=46) ? spacing : spacing-47;
+        if (isalpha(SYS_2[sysi])) sprintf(buffer+strlen(buffer), "%c", (islower(text[txti])) ? SYS_2[sysi] : SYS_2[sysi]-0x20);
+        else sprintf(buffer+strlen(buffer), "%c", SYS_2[sysi]);
         continue;
-      } else ptr = strchr(SYS_2, text[loop]);
+      } else ptr = strchr(SYS_2, text[txti]);
       if (ptr!=NULL) {
-        i = ptr - SYS_2;
-        i += (i+spacing<=46) ? spacing : spacing-47;
-        sprintf(buffer+strlen(buffer), "%c", SYS_2[i]);
-      } else sprintf(buffer+strlen(buffer), "%c", text[loop]);
+        sysi = ptr - SYS_2;
+        sysi += (sysi+spacing<=46) ? spacing : spacing-47;
+        sprintf(buffer+strlen(buffer), "%c", SYS_2[sysi]);
+      } else sprintf(buffer+strlen(buffer), "%c", text[txti]);
     }
-	break;
-	}
+  break;
+  }
 }
 
-//Spacing range:
+//spacing range:
 //SYS_0 (0~25) | SYS_1 (0~35) | SYS_2 (0~46)
 //Any spacing out of boundary may lead to issues
 //The buffer allocation space should match text or greater
 //The memory block of buffer will be reset in its initial execution
 void decrypt(char* text, char* buffer, uint8_t system, uint8_t spacing) {
-	const char* ptr;
-	int i; unsigned int loop;
-	memset(buffer, 0, sizeof(buffer));
-	switch (system) {
-	case 0:
-		for (loop=0; loop<strlen(text); loop++) {
-      if (0x61<=text[loop] && text[loop]<=0x7a) ptr = strchr(SYS_0, text[loop]);
-      else ptr = strchr(SYS_0, text[loop]+0x20);
+  int32_t sysi; 
+  uint32_t txti; /*sysi - SYS index, txti - text index*/
+  const char* ptr;
+  memset(buffer, 0, sizeof(buffer));
+  switch (system) {
+  case 0:
+    for (txti=0; txti<strlen(text); txti++) {
+      if (islower(text[txti])) ptr = strchr(SYS_0, text[txti]);
+      else ptr = strchr(SYS_0, text[txti]+0x20);
       if (ptr!=NULL) {
-      	i = ptr - SYS_0;
-      	i -= (i-spacing>-1) ? spacing : spacing+25;
-        sprintf(buffer+strlen(buffer), "%c", (0x61<=text[loop] && text[loop]<=0x7a) ? SYS_0[i] : SYS_0[i]-0x20);
-      } else sprintf(buffer+strlen(buffer), "%c", text[loop]);
+        sysi = ptr - SYS_0;
+        sysi -= (sysi-spacing>-1) ? spacing : spacing+25;
+        sprintf(buffer+strlen(buffer), "%c", (islower(text[txti])) ? SYS_0[sysi] : SYS_0[sysi]-0x20);
+      } else sprintf(buffer+strlen(buffer), "%c", text[txti]);
     }
-	break;
-	case 1:
-		for (loop=0; loop<strlen(text); loop++) {
-      if (isalpha(text[loop])) {
-      	ptr = strchr(SYS_1, (0x61<=text[loop] && text[loop]<=0x7a) ? text[loop] : text[loop]+0x20);
-      	i = ptr - SYS_1;
-      	i -= (i-spacing>-1) ? spacing : spacing+35;
-        if (isalpha(SYS_1[i])) sprintf(buffer+strlen(buffer), "%c", (0x61<=text[loop] && text[loop]<=0x7a) ? SYS_1[i] : SYS_1[i]-0x20);
-        else sprintf(buffer+strlen(buffer), "%c", SYS_1[i]);
+  break;
+  case 1:
+    for (txti=0; txti<strlen(text); txti++) {
+      if (isalpha(text[txti])) {
+        ptr = strchr(SYS_1, (islower(text[txti])) ? text[txti] : text[txti]+0x20);
+        sysi = ptr - SYS_1;
+        sysi -= (sysi-spacing>-1) ? spacing : spacing+35;
+        if (isalpha(SYS_1[sysi])) sprintf(buffer+strlen(buffer), "%c", (islower(text[txti])) ? SYS_1[sysi] : SYS_1[sysi]-0x20);
+        else sprintf(buffer+strlen(buffer), "%c", SYS_1[sysi]);
         continue;
-      } else ptr = strchr(SYS_1, text[loop]);
+      } else ptr = strchr(SYS_1, text[txti]);
       if (ptr!=NULL) {
-      	i = ptr - SYS_1;
-      	i -= (i-spacing>-1) ? spacing : spacing+35;
-        sprintf(buffer+strlen(buffer), "%c", SYS_1[i]);
-      } else sprintf(buffer+strlen(buffer), "%c", text[loop]);
+        sysi = ptr - SYS_1;
+        sysi -= (sysi-spacing>-1) ? spacing : spacing+35;
+        sprintf(buffer+strlen(buffer), "%c", SYS_1[sysi]);
+      } else sprintf(buffer+strlen(buffer), "%c", text[txti]);
     }
-	break;
-	case 2:
-		for (loop=0; loop<strlen(text); loop++) {
-      if (isalpha(text[loop])) {
-      	ptr = strchr(SYS_2, (0x61<=text[loop] && text[loop]<=0x7a) ? text[loop] : text[loop]+0x20);
-      	i = ptr - SYS_2;
-      	i -= (i-spacing>-1) ? spacing : spacing+47;
-        if (isalpha(SYS_2[i])) sprintf(buffer+strlen(buffer), "%c", (0x61<=text[loop] && text[loop]<=0x7a) ? SYS_2[i] : SYS_2[i]-0x20);
-        else sprintf(buffer+strlen(buffer), "%c", SYS_2[i]);
+  break;
+  case 2:
+    for (txti=0; txti<strlen(text); txti++) {
+      if (isalpha(text[txti])) {
+        ptr = strchr(SYS_2, (islower(text[txti])) ? text[txti] : text[txti]+0x20);
+        sysi = ptr - SYS_2;
+        sysi -= (sysi-spacing>-1) ? spacing : spacing+47;
+        if (isalpha(SYS_2[sysi])) sprintf(buffer+strlen(buffer), "%c", (islower(text[txti])) ? SYS_2[sysi] : SYS_2[sysi]-0x20);
+        else sprintf(buffer+strlen(buffer), "%c", SYS_2[sysi]);
         continue;
-      } else ptr = strchr(SYS_2, text[loop]);
+      } else ptr = strchr(SYS_2, text[txti]);
       if (ptr!=NULL) {
-      	i = ptr - SYS_2;
-      	i -= (i-spacing>-1) ? spacing : spacing+47;
-        sprintf(buffer+strlen(buffer), "%c", SYS_2[i]);
-      } else sprintf(buffer+strlen(buffer), "%c", text[loop]);
+        sysi = ptr - SYS_2;
+        sysi -= (sysi-spacing>-1) ? spacing : spacing+47;
+        sprintf(buffer+strlen(buffer), "%c", SYS_2[sysi]);
+      } else sprintf(buffer+strlen(buffer), "%c", text[txti]);
     }
-	break;
-	}
+  break;
+  }
 }

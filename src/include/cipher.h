f81b58fa1219d63a3808f408e6474c31fcb7bb0c
Vocelia <vocelia@outlook.jp>
2023-09-07 18:24:09 +0300

    Adding a STATE enumeration for simplicity and readability

--- src/include/cipher.h
+++ src/include/cipher.h
@@ -2,5 +2,14 @@
 
 #include <stdint.h>
 
+/*0 = supported special character,
+1 = lowercase alphabetic character,
+2 = uppercase alphabetic character, 
+3 = foreign special character or multi-byte character*/
+enum STATE {
+  SS_CHAR = 0, FS_CHAR = 3,
+  LW_CHAR = 1, UP_CHAR = 2
+};
+
 void encrypt(char* text, char* buffer, uint8_t system, uint8_t spacing);
 void decrypt(char* text, char* buffer, uint8_t system, uint8_t spacing);

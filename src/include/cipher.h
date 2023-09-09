59ea49187c67c26ab062d815bee1e805e0b8a152
Vocelia <vocelia@outlook.jp>
2023-09-09 19:50:47 +0300

    Adding cipher_t typedef to base everything with it

--- src/include/cipher.h
+++ src/include/cipher.h
@@ -12,5 +12,16 @@ enum STATE {
 	INT_CHAR = 0, SS_CHAR = 3, FS_CHAR = 4
 };
 
+typedef struct {
+	char* text;
+	char* buffer;
+	uint32_t buff_len;
+	enum STATE state;
+	uint32_t sysi; /*sysi - SYS index*/
+	uint32_t txti; /*txti - text index*/
+	uint8_t system;
+	uint8_t spacing;
+} cipher_t;
+
 void encrypt(char* text, char* buffer, uint8_t system, uint8_t spacing);
 void decrypt(char* text, char* buffer, uint8_t system, uint8_t spacing);

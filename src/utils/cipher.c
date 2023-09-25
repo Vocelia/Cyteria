4af2bb692385343a2d7befdd5588ea0b7520fea2
Vocelia <vocelia@outlook.jp>
2023-09-25 18:01:44 +0300

    Rearranging the file structure

---
+++ src/utils/cipher.c
@@ -0,0 +1,252 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
+#include "include/cipher.h"
+
+/*Supported special characters*/
+static const char* SSC = " .,!?*$#@%&";
+
+/*All system variables the cipher depends on*/
+static const char* SYS_0 = "abcdefghijklmnopqrstuvwxyz";
+static const char* SYS_1 = "abcd01efgh45ijkl23mnopqr89stuv6wxyz7";
+static const char* SYS_2 = "ab cd01ef?,gh45ij@#$kl23mn.%&opqr89stuv6wx!yz7*";
+
+/*Hardcoded SYS lengths to avoid unnecessary computations*/
+static const uint8_t SYS_0_len = 26;
+static const uint8_t SYS_1_len = 36;
+static const uint8_t SYS_2_len = 47;
+
+/*Dumps the specified character to buffer and advances the buffer pointer */
+static void charDump(char* buffer, uint32_t* buff_len, const char _char) {
+  uint32_t buff_len_val = *(buff_len);
+  sprintf(buffer+buff_len_val, "%c", _char);
+  buff_len_val++; /*Advances the pointer to the next memory block*/
+  *(buff_len) = buff_len_val;
+}
+
+/*Returns SYS index through pointer arithmetic operation*/
+static uint32_t getSYSIndex(const char* SYS, const char _char) {
+  const char* ptr = strchr(SYS, _char);
+  return ptr - SYS;
+}
+
+/*Returns the state according to the index of the text array
+Note: The if-statements are arranged in a probabilistic way*/
+static enum STATE getState(char* text, uint32_t txti) {
+  if (0x61<=text[txti] && text[txti]<=0x7a) return LW_CHAR;
+  else if (0x41<=text[txti] && text[txti]<=0x5a) return UP_CHAR;
+  else if (0x30<=text[txti] && text[txti]<=0x39) return INT_CHAR;
+  else if (strchr(SSC, text[txti])!=NULL) return SS_CHAR;
+  else return FS_CHAR;
+}
+
+/*Encodes a character according to the given case
+All cases:
+  0 = encode (normal),
+  1 = encode (uppercase),
+  2 = ignore*/
+static void encode(cipher_t* cipher_ptr, uint8_t _case, const char* SYS, const uint8_t SYS_len) {
+  cipher_t cipher = *(cipher_ptr);
+  switch (_case) {
+    case 0:
+      cipher.sysi = getSYSIndex(SYS, cipher.text[cipher.txti]);
+      /*Encodes character by moving pointers*/
+      cipher.sysi = (cipher.steps+cipher.sysi)%SYS_len;
+      charDump(cipher.buffer, &cipher.buff_len, SYS[cipher.sysi]);
+      break;
+    case 1:
+      cipher.sysi = getSYSIndex(SYS, cipher.text[cipher.txti]+0x20);
+      /*Encodes character by moving pointers*/
+      cipher.sysi = (cipher.steps+cipher.sysi)%SYS_len;
+      charDump(cipher.buffer, &cipher.buff_len, toupper(SYS[cipher.sysi]));
+      break;
+    case 2:
+      cipher.sysi = getSYSIndex(SYS, cipher.text[cipher.txti]);
+      charDump(cipher.buffer, &cipher.buff_len, cipher.text[cipher.txti]);
+      break;
+  } *(cipher_ptr) = cipher;
+}
+
+/*Decodes a character according to the given case
+All cases:
+  0 = encode (normal),
+  1 = encode (uppercase),
+  2 = ignore*/
+static void decode(cipher_t* cipher_ptr, uint8_t _case, const char* SYS, const uint8_t SYS_len) {
+  int32_t decoded_pos;
+  cipher_t cipher = *(cipher_ptr);
+  switch (_case) {
+    case 0:
+      cipher.sysi = getSYSIndex(SYS, cipher.text[cipher.txti]);
+      /*Decodes character by calculating positions through steps*/
+      decoded_pos = cipher.sysi-cipher.steps;
+      if (decoded_pos<0) cipher.sysi = SYS_len+decoded_pos;
+      else cipher.sysi = decoded_pos%SYS_len;
+      charDump(cipher.buffer, &cipher.buff_len, SYS[cipher.sysi]);
+      break;
+    case 1:
+      cipher.sysi = getSYSIndex(SYS, cipher.text[cipher.txti]+0x20);
+      /*Decodes character by calculating positions through steps*/
+      decoded_pos = cipher.sysi-cipher.steps;
+      if (decoded_pos<0) cipher.sysi = SYS_len+decoded_pos;
+      else cipher.sysi = decoded_pos%SYS_len;
+      charDump(cipher.buffer, &cipher.buff_len, toupper(SYS[cipher.sysi]));
+      break;
+    case 2:
+      cipher.sysi = getSYSIndex(SYS, cipher.text[cipher.txti]);
+      charDump(cipher.buffer, &cipher.buff_len, cipher.text[cipher.txti]);
+      break;
+  } *(cipher_ptr) = cipher;
+}
+
+/*Encrypts given text to buffer according to system and steps
+Steps' ranges are:
+SYS_0 (0~26) | SYS_1 (0~36) | SYS_2 (0~47)
+Any steps out of boundary will lead to unexpected exceptions*/
+void encrypt(char* text, char* buffer, uint8_t system, uint8_t steps) {
+  /*Initialising cipher_t and its variables*/
+  cipher_t cipher;
+  cipher.txti = 0;
+  cipher.sysi = 0;
+  cipher.text = text;
+  cipher.buffer = buffer;
+  cipher.system = system;
+  cipher.steps = steps;
+  cipher.buff_len = strlen(buffer);
+  switch (system) {
+    case 0:
+      for (; cipher.txti<strlen(text); cipher.txti++) {
+        cipher.state = getState(text, cipher.txti);
+        switch (cipher.state) {
+          case LW_CHAR:
+            encode(&cipher, 0, SYS_0, SYS_0_len);
+            break;
+          case UP_CHAR:
+            encode(&cipher, 1, SYS_0, SYS_0_len);
+            break;
+          default:
+            encode(&cipher, 2, SYS_0, SYS_0_len);
+            break;
+        }
+      }
+      break;
+    case 1:
+      for (; cipher.txti<strlen(text); cipher.txti++) {
+        cipher.state = getState(text, cipher.txti);
+        switch (cipher.state) {
+          case LW_CHAR:
+            encode(&cipher, 0, SYS_1, SYS_1_len);
+            break;
+          case UP_CHAR:
+            encode(&cipher, 1, SYS_1, SYS_1_len);
+            break;
+          case INT_CHAR:
+            encode(&cipher, 0, SYS_1, SYS_1_len);
+            break;
+          default:
+            encode(&cipher, 2, SYS_1, SYS_1_len);
+            break;
+        }
+      }
+      break;
+    case 2:
+      for (; cipher.txti<strlen(text); cipher.txti++) {
+        cipher.state = getState(text, cipher.txti);
+        switch (cipher.state) {
+          case LW_CHAR:
+            encode(&cipher, 0, SYS_2, SYS_2_len);
+            break;
+          case UP_CHAR:
+            encode(&cipher, 1, SYS_2, SYS_2_len);
+            break;
+          case INT_CHAR:
+            encode(&cipher, 0, SYS_2, SYS_2_len);
+            break;
+          case SS_CHAR:
+            encode(&cipher, 0, SYS_2, SYS_2_len);
+            break;
+          default:
+            encode(&cipher, 2, SYS_2, SYS_2_len);
+            break;
+        }
+      }
+      break;
+  }
+}
+
+/*Decrypts given text to buffer according to system and steps
+Steps' ranges are:
+SYS_0 (0~26) | SYS_1 (0~36) | SYS_2 (0~47)
+Any steps out of boundary will lead to unexpected exceptions*/
+void decrypt(char* text, char* buffer, uint8_t system, uint8_t steps) {
+  /*Initialising cipher_t and its variables*/
+  cipher_t cipher;
+  cipher.txti = 0;
+  cipher.sysi = 0;
+  cipher.text = text;
+  cipher.buffer = buffer;
+  cipher.system = system;
+  cipher.steps = steps;
+  cipher.buff_len = strlen(buffer);
+  switch (system) {
+    case 0:
+      for (; cipher.txti<strlen(text); cipher.txti++) {
+        cipher.state = getState(text, cipher.txti);
+        switch (cipher.state) {
+          case LW_CHAR:
+            decode(&cipher, 0, SYS_0, SYS_0_len);
+            break;
+          case UP_CHAR:
+            decode(&cipher, 1, SYS_0, SYS_0_len);
+            break;
+          default:
+            decode(&cipher, 2, SYS_0, SYS_0_len);
+            break;
+        }
+      }
+      break;
+    case 1:
+      for (; cipher.txti<strlen(text); cipher.txti++) {
+        cipher.state = getState(text, cipher.txti);
+        switch (cipher.state) {
+          case LW_CHAR:
+            decode(&cipher, 0, SYS_1, SYS_1_len);
+            break;
+          case UP_CHAR:
+            decode(&cipher, 1, SYS_1, SYS_1_len);
+            break;
+          case INT_CHAR:
+            decode(&cipher, 0, SYS_1, SYS_1_len);
+            break;
+          default:
+            decode(&cipher, 2, SYS_1, SYS_1_len);
+            break;
+        }
+      }
+      break;
+    case 2:
+      for (; cipher.txti<strlen(text); cipher.txti++) {
+        cipher.state = getState(text, cipher.txti);
+        switch (cipher.state) {
+          case LW_CHAR:
+            decode(&cipher, 0, SYS_2, SYS_2_len);
+            break;
+          case UP_CHAR:
+            decode(&cipher, 1, SYS_2, SYS_2_len);
+            break;
+          case INT_CHAR:
+            decode(&cipher, 0, SYS_2, SYS_2_len);
+            break;
+          case SS_CHAR:
+            decode(&cipher, 0, SYS_2, SYS_2_len);
+            break;
+          default:
+            decode(&cipher, 2, SYS_2, SYS_2_len);
+            break;
+        }
+      }
+      break;
+  }
+}
---
+++ src/utils/stegano.c
@@ -0,0 +1,136 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include "include/stegano.h"
+
+static const char* SIG = "CYT"; //signature
+static void readFromLSBs(stegano_t* info_ptr, char* container, uint32_t margin);
+
+/* Checks the requirements by msg in reference to data
+Returns a positive number if data is enough to satisfy msg */
+int32_t check_limit(stegano_t info) {
+	return info.data_len-((info.msg_len*8)+65); /*65 bits for the header*/
+}
+
+/* Checks for the validaty of the data stream */
+static bool check_sig(stegano_t info) {
+    info.cur = 0;
+    info.offset = 24; /* 3*8 bits */
+    char* intro = (char*)malloc(4*sizeof(char));
+    memset(intro, 0, 4*sizeof(char)); /*Memory allocation for members*/
+    readFromLSBs(&info, intro, 8);
+    intro[3] = '\0'; /*null-terminate intro to avoid unexpected outcomes*/
+    bool rtn = (strcmp(SIG, intro) == 0) ? true : false;
+    free(intro);
+    return rtn;
+}
+
+/* Reads from the Least Significant Bit (LSB) of data, writing to container until reaching offset
+Given a margin, container is used as an array split with the specified margin. */
+static void readFromLSB(stegano_t* info_ptr, uint32_t* container) {
+    bool bit;
+    stegano_t info = *(info_ptr);
+    *(container) = 0; /* Fallback for unallocated container */ 
+    for (; info.cur<info.offset; info.cur++) {
+        bit = info.data[info.cur] >> 0 & 1; //gets the LSBs of data
+        if (bit) *(container) += pow(2, ((info.offset-1) - info.cur)); //addition by 2^index
+    } *(info_ptr) = info;
+}
+
+static void readFromLSBs(stegano_t* info_ptr, char* container, uint32_t margin) {
+    bool bit;
+    stegano_t info = *(info_ptr);
+    uint32_t j = 0, bitcount = margin, k = info.cur+(margin-1);
+    for (; info.cur<info.offset; info.cur++) {
+        bitcount--;
+        bit = info.data[info.cur] >> 0 & 1; //gets the LSBs of data
+        if (bit) container[j] += pow(2, bitcount); //addition by 2^index
+        if (info.cur == k) { //container shifts into the next member each margin cycle
+            j++; k += margin; bitcount = margin;
+        }
+    } *(info_ptr) = info;
+}
+
+/* Writes bits to the Least Significant Bit (LSB) of data until it reaches the specified offset
+Given a margin, msg is used as an array split with the specified margin. */
+static void writeToLSB(stegano_t* info_ptr, const char bits) {
+    bool bit;
+    stegano_t info = *(info_ptr);
+    for (; info.cur<info.offset; info.cur++) {
+        bit = bits >> ((info.offset-1) - info.cur) & 1; //gets the LSB of bits
+        if ((info.data[info.cur]%2) == 0) info.data[info.cur] += (bit) ? 1 : 0;
+        else info.data[info.cur] -= (bit) ? 0 : 1;
+    } *(info_ptr) = info;
+}
+
+static void writeToLSBs(stegano_t* info_ptr, uint32_t margin) {
+    bool bit;
+    stegano_t info = *(info_ptr);
+    uint32_t j = 0, bitcount = margin, k = info.cur+(margin-1);
+    for (; info.cur<info.offset; info.cur++) {
+        bitcount--;
+        bit = info.msg[j] >> bitcount & 1; //gets the LSB of bits[j]
+        if ((info.data[info.cur]%2) == 0) info.data[info.cur] += (bit) ? 1 : 0;
+        else info.data[info.cur] -= (bit) ? 0 : 1;
+        if (info.cur == k) { //msg shifts into the next member each margin cycle
+            j++; k += margin; bitcount = margin;
+        }
+    } *(info_ptr) = info;
+}
+    
+/* Hides a message (msg) within data through its Least Significant Bit (LSB)
+Requirements: info.data, info.msg, info.msg_len, info.data_len */
+bool hide(stegano_t* info_ptr, uint8_t system, uint8_t spacing) {
+    stegano_t info = *(info_ptr);
+    const char* msg = info.msg; /* msg pointer as info.msg will be overwritten with SIG */
+    if (check_limit(info)>0) {
+        /* Header: signature, system, spacing, length */
+        info.cur = 0;
+        info.offset = 0;
+        info.msg = SIG;
+        info.offset += 24;
+        writeToLSBs(&info, 8);
+        info.offset += 3;
+        writeToLSB(&info, system);
+        info.offset += 6;
+        writeToLSB(&info, spacing);
+        info.offset += 32;
+        writeToLSB(&info, info.msg_len);
+        /* Body: message */
+        info.msg = msg;
+        info.offset += info.msg_len*8;
+        writeToLSBs(&info, 8);
+        *(info_ptr) = info;
+    } else return false;
+    return true;
+}
+
+/* Reveals a message (msg) within data through its Least Significant Bit (LSB)
+Requirements: info.data, buffer, info.msg_len, info.data_len */
+bool reveal(stegano_header_t* header, stegano_t* info_ptr, char* buffer) {
+    stegano_t info = *(info_ptr);
+    stegano_header_t head = *(header);
+    if (check_sig(info)) {
+        /* SIG is skipped */
+        info.cur = 24;
+        info.offset = 27;
+        /* Header: system, spacing, length */
+        readFromLSB(&info, (uint32_t*)&head.system);
+        info.offset += 6;
+        readFromLSB(&info, (uint32_t*)&head.spacing);
+        info.offset += 32;
+        readFromLSB(&info, &head.len);
+        /* Body: message */
+        info.offset += head.len*8;
+        /*Resetting buffer memory block to avoid unexpected outcome*/
+        memset(buffer, 0, (head.len+1)*sizeof(char)); /*+1 to reserve for null terminator*/ 
+        readFromLSBs(&info, buffer, 8);
+        *(header) = head;
+        *(info_ptr) = info;
+    } else return false;
+    return true;
+}
